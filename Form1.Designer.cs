387cc8ec0eb85da5f4946c780a94c835ca238c53
LIuba <you@example.com>
2017-10-14 19:30:41 +0300

    Init commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿using System.Windows.Forms;
+namespace AppTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            try
+            {
+                if (disposing && (components != null))
+                {
+                    components.Dispose();
+                }
+                base.Dispose(disposing);
+            }
+
+	        catch (System.Exception e)
+	        {
+
+                MessageBox.Show("Пожалуйста, приложите скриншот или текст этого отчета в письме технической поддержке или преподавателю\n\n" + e.Message);
+	        }
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.load_from_file = new System.Windows.Forms.Button();
+            this.load_from_server = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // load_from_file
+            // 
+            this.load_from_file.Location = new System.Drawing.Point(48, 25);
+            this.load_from_file.Name = "load_from_file";
+            this.load_from_file.Size = new System.Drawing.Size(198, 47);
+            this.load_from_file.TabIndex = 0;
+            this.load_from_file.Text = "Загрузить из файла";
+            this.load_from_file.UseVisualStyleBackColor = true;
+            this.load_from_file.Click += new System.EventHandler(this.load_from_file_Click);
+            // 
+            // load_from_server
+            // 
+            this.load_from_server.Location = new System.Drawing.Point(48, 92);
+            this.load_from_server.Name = "load_from_server";
+            this.load_from_server.Size = new System.Drawing.Size(198, 46);
+            this.load_from_server.TabIndex = 1;
+            this.load_from_server.Text = "Загрузить с сервера (автоматически)";
+            this.load_from_server.UseMnemonic = false;
+            this.load_from_server.UseVisualStyleBackColor = true;
+            this.load_from_server.Click += new System.EventHandler(this.load_from_server_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(294, 159);
+            this.Controls.Add(this.load_from_server);
+            this.Controls.Add(this.load_from_file);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Certificate Importer";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button load_from_file;
+        private System.Windows.Forms.Button load_from_server;
+
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Windows.Forms;
+using System.IO;
+using System.Net;
+using System.Security.Cryptography.X509Certificates;
+
+namespace AppTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private void load_from_file_Click(object sender, EventArgs e)
+
+        {  
+            try
+            {        
+                OpenFileDialog openFileDialog = new OpenFileDialog();
+                openFileDialog.Multiselect = false;
+                openFileDialog.Filter = "x509 certificate (*.cer; *.crt)|*.cer;*.crt";
+                openFileDialog.ShowDialog();
+                var cert_paths = openFileDialog.FileNames[0];    
+                if (cert_paths.Equals(""))
+                {
+                    int num = (int)MessageBox.Show("Выберите сертификат!");
+                }
+                else
+                {
+                        this.import_cert(cert_paths);
+                }
+            }
+            catch (System.Exception exf)
+	        {
+
+                MessageBox.Show("Пожалуйста, приложите скриншот или текст этого отчета в письме технической поддержке или преподавателю\n\n" + exf.Message + "\nin load from file procedure");
+	        }
+        }
+
+        private void load_from_server_Click(object sender, EventArgs e)
+        {
+            try
+            { 
+                Directory.CreateDirectory("certificates_avalon");
+                WebClient webClient = new WebClient();
+                webClient.DownloadFile("http://www.avalon.ru/ca/AVALON CA.crt", "AVALON_CA.crt");
+                this.import_cert("AVALON_CA.crt");
+            }
+            catch (System.Exception exs)
+            {
+
+                MessageBox.Show("Пожалуйста, приложите скриншот или текст этого отчета в письме технической поддержке или преподавателю\n\n" + exs.Message + "\nin load from server procedure");
+            }
+        }
+        public void import_cert(string path)
+        {
+            try
+            {
+                X509Certificate2 x509Certificate2 = new X509Certificate2(path);
+                X509Store x509Store = new X509Store(StoreName.Root, StoreLocation.LocalMachine);
+                x509Store.Open(OpenFlags.ReadWrite);
+                foreach (X509Certificate2 certificate in x509Store.Certificates)
+                {
+                    if (certificate.SerialNumber == x509Certificate2.SerialNumber)
+                    {
+                        int num = (int)MessageBox.Show("Сертификат уже установлен!");
+                        x509Store.Close();
+                        return;
+                    }
+                }
+                x509Store.Add(new X509Certificate2(X509Certificate.CreateFromCertFile(path)));
+                foreach (X509Certificate2 certificate in x509Store.Certificates)
+                {
+                    if (certificate.SerialNumber == x509Certificate2.SerialNumber)
+                    {
+                        int num = (int)MessageBox.Show("Установка сертификата выполнена успешно!");
+                        x509Store.Close();
+                        return;
+                    }
+                }
+                int num1 = (int)MessageBox.Show("Неизвестная ошибка! Сертификат отсутствует!");
+                x509Store.Close();
+            }
+            catch (System.Exception exi)
+            {
+
+                MessageBox.Show("Пожалуйста, приложите скриншот или текст этого отчета в письме технической поддержке или преподавателю\n\n" + exi.Message + "\nin import procedure");
+            }
+        }
+    }
+}
